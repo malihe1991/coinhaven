a7bd4e311298dc06ca537f99fcf076839c75b2f3
malihe.ghaderi <maliheghaderri@gmail.com>
2022-07-17 22:50:31 +0430

    feat(layout.svelte): :sparkles: Add layout component

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,14 @@
+<script>
+  import "../styles/app.css";
+</script>
+
+<div class="container m-auto p-5">
+  <slot />
+</div>
+
+<style>
+  .container {
+    width: 80%;
+    min-height: 100vh;
+  }
+</style>
